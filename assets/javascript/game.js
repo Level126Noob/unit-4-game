07098ada10b00f93ab4d88f78753710064f8dc97
travis <abe.travis@outlook.com>
2019-05-29 18:28:28 -0700

    Linked jquery, added css styles

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+//declaring variables for the game
+var hp = 99;
+var basehit = undefined;
+var computerhit = undefined;
+var opponents = [];
+var defender;
+var player;
+var pselected = false;
+var defselected = false;
+
+function attack(min, max) {
+    return Math.floor(Math.random() * (35 - 1 + 1) ) + 1;
+    console.log();
+  }
+
+});
