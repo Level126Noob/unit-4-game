845928792f933f3734dabb2e7f6d57c243076c53
travis <abe.travis@outlook.com>
2019-05-30 17:41:38 -0700

    Changed CSS to match, Declared variables propperly, got console.log of rng to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,46 @@
-$( document ).ready(function() {
-//declaring variables for the game
-var hp = 99;
-var basehit = undefined;
-var computerhit = undefined;
-var opponents = [];
-var defender;
-var player;
-var pselected = false;
-var defselected = false;
-
-function attack(min, max) {
-    return Math.floor(Math.random() * (35 - 1 + 1) ) + 1;
-    console.log();
-  }
-
+$(document).ready(function () {
+  //declaring the variables and the character objects
+  var hp = 99;
+  playercharacter = false;
+  enemyselected = false;
+  defense = undefined;
+  var attack = Math.floor(Math.random() * 31 + 1)
+  console.log(attack);
+  
+  var Ahrim = {
+    hp: 99,
+    name: "Ahrim",
+    defense: 17,
+    imageurl: "assets/images/Ahrim.png"
+  };
+  var Dharok = {
+    hp: 99,
+    name: "Dharok",
+    defense: 22,
+    imageurl: "assets/images/Ahrim.png"
+  };
+  var Guthan = {
+    hp: 99,
+    name: "Guthan",
+    defense: 9,
+    imageurl: "assets/images/Ahrim.png"
+  };
+  var Karil = {
+    hp: 99,
+    name: "Karil",
+    defense: 12,
+    imageurl: "assets/images/Ahrim.png"
+  };
+  var Torag = {
+    hp: 99,
+    name: "Torag",
+    defense: 11,
+    imageurl: "assets/images/Ahrim.png"
+  };
+  var Verac = {
+    hp: 99,
+    name: "Verac",
+    defense: 15,
+    imageurl: "assets/images/Ahrim.png"
+  };
 });
