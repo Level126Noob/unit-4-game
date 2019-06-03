914b8ced5c9334d4406d386eab0ff81f0652c3ba
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-06-02 21:27:34 -0400

    added divs for each character, added onclick divs with character object image, name, and hp

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,64 @@
 $(document).ready(function () {
-  //declaring the variables and the character objects
-  var hp = 99;
-  playercharacter = false;
-  enemyselected = false;
-  defense = undefined;
-  var attack = Math.floor(Math.random() * 31 + 1)
-  console.log(attack);
-  
+
+  //defining the character objects
   var Ahrim = {
+    Name: "Ahrim",
+    defense: 12,
     hp: 99,
-    name: "Ahrim",
-    defense: 17,
-    imageurl: "assets/images/Ahrim.png"
+    image: "<img src='assets/images/Ahrim.png' />",
   };
   var Dharok = {
+    Name: "Dharok",
+    defense: 14,
     hp: 99,
-    name: "Dharok",
-    defense: 22,
-    imageurl: "assets/images/Ahrim.png"
+    image: "<img src = 'assets/images/Dharok.png' />",
   };
   var Guthan = {
+    Name: "Guthan",
+    defense: 8,
     hp: 99,
-    name: "Guthan",
-    defense: 9,
-    imageurl: "assets/images/Ahrim.png"
+    image: "<img src='assets/images/Guthan.png' />",
   };
   var Karil = {
+    Name: "Karil",
+    defense: 8,
     hp: 99,
-    name: "Karil",
-    defense: 12,
-    imageurl: "assets/images/Ahrim.png"
+    image: "<img src='assets/images/Karil.png' />",
   };
   var Torag = {
+    Name: "Torag",
+    defense: 7,
     hp: 99,
-    name: "Torag",
-    defense: 11,
-    imageurl: "assets/images/Ahrim.png"
+    image: "<img src='assets/images/Torag.png' />",
   };
   var Verac = {
+    Name: "Verac",
+    defense: 12,
     hp: 99,
-    name: "Verac",
-    defense: 15,
-    imageurl: "assets/images/Ahrim.png"
+    image: "<img src='assets/images/Verac.png' />",
   };
+
+  //defining the attack
+  var attack = Math.floor(Math.random() * 31 + 1)
+  console.log(attack);
+
+  //onclick function
+  $(".AhrimIMG").click(function () {
+    document.getElementById("your character").innerHTML ='<div>' + Ahrim.image + '<p>Name: ' + Ahrim.Name + '</p><p>Health: ' + Ahrim.hp + '</p></div>' + "<h1>Your Character</h1>"
+  });
+  $(".DharokIMG").click(function () {
+    document.getElementById("your character").innerHTML = '<div>' + Dharok.image + '<p>Name: ' + Dharok.Name + '</p><p>Health: ' + Dharok.hp + '</p></div>' + "<h1>Your Character</h1>"
+  });
+  $(".GuthanIMG").click(function () {
+    document.getElementById("your character").innerHTML = '<div>' + Guthan.image + '<p>Name: ' + Guthan.Name + '</p><p>Health: ' + Guthan.hp + '</p></div>' + "<h1>Your Character</h1>"
+  });
+  $(".KarilIMG").click(function () {
+    document.getElementById("your character").innerHTML = '<div>' + Karil.image + '<p>Name: ' + Karil.Name + '</p><p>Health: ' + Karil.hp + '</p></div>' + "<h1>Your Character</h1>"
+  });
+  $(".ToragIMG").click(function () {
+    document.getElementById("your character").innerHTML = '<div>' + Torag.image + '<p>Name: ' + Torag.Name + '</p><p>Health: ' + Torag.hp + '</p></div>' + "<h1>Your Character</h1>"
+  });
+  $(".VeracIMG").click(function () {
+    document.getElementById("your character").innerHTML = '<div>' + Verac.image + '<p>Name: ' + Verac.Name + '</p><p>Health: ' + Verac.hp + '</p></div>' + "<h1>Your Character</h1>"
+  });
 });
