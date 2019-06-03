058b92aa3fad6a849fe3e682759b4aa1a15f25e5
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-06-02 23:59:14 -0400

    Ahrim.selected = true;

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,90 @@
 $(document).ready(function () {
-
+ //defining local variables
+  selected = false;
   //defining the character objects
   var Ahrim = {
     Name: "Ahrim",
     defense: 12,
     hp: 99,
     image: "<img src='assets/images/Ahrim.png' />",
+    selected: false,
   };
   var Dharok = {
     Name: "Dharok",
     defense: 14,
     hp: 99,
     image: "<img src = 'assets/images/Dharok.png' />",
+    selected: false,
   };
   var Guthan = {
     Name: "Guthan",
     defense: 8,
     hp: 99,
     image: "<img src='assets/images/Guthan.png' />",
+    selected: false,
   };
   var Karil = {
     Name: "Karil",
     defense: 8,
     hp: 99,
     image: "<img src='assets/images/Karil.png' />",
+    selected: false,
   };
   var Torag = {
     Name: "Torag",
     defense: 7,
     hp: 99,
     image: "<img src='assets/images/Torag.png' />",
+    selected: false,
   };
   var Verac = {
     Name: "Verac",
     defense: 12,
     hp: 99,
     image: "<img src='assets/images/Verac.png' />",
+    selected: false,
   };
 
   //defining the attack
   var attack = Math.floor(Math.random() * 31 + 1)
   console.log(attack);
 
-  //onclick function
+  //onclick for placing character in the your character div and changing variable for if character has been selected.
   $(".AhrimIMG").click(function () {
-    document.getElementById("your character").innerHTML ='<div>' + Ahrim.image + '<p>Name: ' + Ahrim.Name + '</p><p>Health: ' + Ahrim.hp + '</p></div>' + "<h1>Your Character</h1>"
+    document.getElementById("character").innerHTML = '<div class = "ahrim"><h3>Name: ' + Ahrim.Name + '</h3>' + Ahrim.image + '<h4>Health: ' + Ahrim.hp + '</h4>' + "<h1>Your Character</h1></div>";
+    document.getElementById("enemies").innerHTML = '<div class = "verac"><h3>Name: ' + Verac.Name + '</h3>' + Verac.image + '<h4>Health: ' + Verac.hp + '</div></h4>' + '<div class = "dharok"><h3>Name: ' + Dharok.Name + '</h3>' + Dharok.image + '<h4>Health: ' + Dharok.hp + '</h4></div>' + 
+    '<div class = "guthan"><h3>Name: ' + Guthan.Name + '</h3>' + Guthan.image + '<h4>Health: ' + Guthan.hp + '</h4></div>'+ '<div class = "karil"><h3>Name: ' + Karil.Name + '</h3>' + Karil.image + '<h4>Health: ' + Karil.hp + '</h4></div>' + 
+    '<div class = "torag"><h3>Name: ' + Torag.Name + '</h3>' + Torag.image + '<h4>Health: ' + Torag.hp + '</h4></div>' + "<h1>Enemies Avaliable To Attack</h1>";
+    $(".AhrimIMG").remove(".AhrimIMG")
+    Ahrim.selected === true;
   });
   $(".DharokIMG").click(function () {
-    document.getElementById("your character").innerHTML = '<div>' + Dharok.image + '<p>Name: ' + Dharok.Name + '</p><p>Health: ' + Dharok.hp + '</p></div>' + "<h1>Your Character</h1>"
+    document.getElementById("character").innerHTML = '<div><h3>Name: ' + Dharok.Name + '</h3>' + Dharok.image + '<h4>Health: ' + Dharok.hp + '</h4></div>' + "<h1>Your Character</h1>";
+    $(".DharokIMG").remove(".DharokIMG");
+    Dharok.selected === true;
   });
   $(".GuthanIMG").click(function () {
-    document.getElementById("your character").innerHTML = '<div>' + Guthan.image + '<p>Name: ' + Guthan.Name + '</p><p>Health: ' + Guthan.hp + '</p></div>' + "<h1>Your Character</h1>"
+    document.getElementById("character").innerHTML = '<div><h3>Name: ' + Guthan.Name + '</h3>' + Guthan.image + '<h4>Health: ' + Guthan.hp + '</h4></div>' + "<h1>Your Character</h1>";
+    $(".GuthanIMG").remove(".GuthanIMG");
+    Guthan.selected === true;
   });
   $(".KarilIMG").click(function () {
-    document.getElementById("your character").innerHTML = '<div>' + Karil.image + '<p>Name: ' + Karil.Name + '</p><p>Health: ' + Karil.hp + '</p></div>' + "<h1>Your Character</h1>"
+    document.getElementById("character").innerHTML = '<div><h3>Name: ' + Karil.Name + '</h3>' + Karil.image + '<h4>Health: ' + Karil.hp + '</h4></div>' + "<h1>Your Character</h1>";
+    $(".KarilIMG").remove(".KarilIMG");
+    Karil.selected === true;
   });
   $(".ToragIMG").click(function () {
-    document.getElementById("your character").innerHTML = '<div>' + Torag.image + '<p>Name: ' + Torag.Name + '</p><p>Health: ' + Torag.hp + '</p></div>' + "<h1>Your Character</h1>"
+    document.getElementById("character").innerHTML = '<div><h3>Name: ' + Torag.Name + '</h3>' + Torag.image + '<h4>Health: ' + Torag.hp + '</h4></div>' + "<h1>Your Character</h1>";
+    $(".ToragIMG").remove(".ToragIMG");
+    Torag.selected === true;
   });
   $(".VeracIMG").click(function () {
-    document.getElementById("your character").innerHTML = '<div>' + Verac.image + '<p>Name: ' + Verac.Name + '</p><p>Health: ' + Verac.hp + '</p></div>' + "<h1>Your Character</h1>"
+    document.getElementById("character").innerHTML = '<div><h3>Name: ' + Verac.Name + '</h3>' + Verac.image + '<h4>Health: ' + Verac.hp + '</h4></div>' + "<h1>Your Character</h1>";
+    $(".VeracIMG").remove(".VeracIMG");
+    Verac.selected === true;
   });
+
+  if (Ahrim.selected = false) {
+
+  };
 });
